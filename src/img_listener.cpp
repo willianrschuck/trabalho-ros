50b2dbd973bf3b27cf8c25b723ec0d814f164fe0
Willian Schuck <willianrschuck@gmail.com>
2021-09-07 22:36:36 -0300

    Finalização da identificação do comando através de histogramas

--- src/img_listener.cpp
+++ src/img_listener.cpp
@@ -2,24 +2,88 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <std_msgs/Int64.h>
+
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+std::vector<cv::Mat> imagens;
+ros::Publisher *pub;
+ros::Rate *rate;
+
+cv::Mat calcularHistograma(cv::Mat mat);
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 	try {
-		cv::imshow("view",cv_bridge::toCvShare(msg,"bgr8")->image);
-		ROS_INFO("EU RECEBI A IMAGEM!!!");
-		cv::waitKey(30);
+		
+		cv::Mat image = cv_bridge::toCvShare(msg,"bgr8")->image;
+		cv::Mat hist = calcularHistograma(image);
+
+		rate->sleep();
+
+		unsigned int i = 0;
+		for (auto hist_base : imagens) {
+			double result = cv::compareHist(hist_base, hist, 1);
+			if (!result) {
+				std_msgs::Int64 response;
+				response.data = i;
+				pub->publish(response);
+				std::cout << i << std::endl;
+				break;
+			}
+			i++;
+		}
+
 	} catch(cv_bridge::Exception& e) {
 		ROS_ERROR("Could not convert from '%s' to 'bgr8'.",msg->encoding.c_str());
 	}
 }
 
+cv::Mat calcularHistograma(cv::Mat mat) {
+
+	cv::Mat hsv_mat;
+	cv::cvtColor(mat, hsv_mat, cv::COLOR_BGR2HSV);
+
+    cv::Mat hsv_half_down = hsv_mat( cv::Range( hsv_mat.rows/2, hsv_mat.rows ), cv::Range( 0, hsv_mat.cols ) );
+
+    int histSize[] = { 50, 60 };
+    float h_ranges[] = { 0, 180 };
+    float s_ranges[] = { 0, 256 };
+
+    const float* ranges[] = { h_ranges, s_ranges };
+
+    int channels[] = { 0, 1 };
+	
+    cv::Mat hist_base, hist_half_down;
+
+    calcHist( &hsv_mat, 1, channels, cv::Mat(), hist_base, 2, histSize, ranges, true, false );
+    normalize( hist_base, hist_base, 0, 1, cv::NORM_MINMAX, -1, cv::Mat() );
+
+	return hist_base;
+
+}
+
 int main(int argc, char **argv){
 	ros::init(argc,argv, "image_listener");
 	ros::NodeHandle nh;
-	cv::namedWindow("view");
+
+	/* THIS IS BAD AND WE NEED TO CHANGE IT */
+    imagens.push_back(calcularHistograma(cv::imread("/home/willian/catkin_ws/src/trabalho/src/images/vazia.png",    cv::IMREAD_COLOR)));
+    imagens.push_back(calcularHistograma(cv::imread("/home/willian/catkin_ws/src/trabalho/src/images/frente.png",   cv::IMREAD_COLOR)));
+    imagens.push_back(calcularHistograma(cv::imread("/home/willian/catkin_ws/src/trabalho/src/images/esquerda.png", cv::IMREAD_COLOR)));
+    imagens.push_back(calcularHistograma(cv::imread("/home/willian/catkin_ws/src/trabalho/src/images/direita.png",  cv::IMREAD_COLOR)));
+
 	
 	image_transport::ImageTransport it(nh);
-	image_transport::Subscriber sub = it.subscribe("camera/image",1,imageCallback);
+	image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+
+	ros::Publisher thePub = nh.advertise<std_msgs::Int64>("result", 1);
+	pub = &thePub;
+	ros::Rate time_rate(2);
+	rate = &time_rate;
+	
 	ros::spin();
-	cv::destroyWindow("view");
+
 }
--- src/robot.cpp
+++ src/robot.cpp
@@ -2,20 +2,49 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <std_msgs/Int64.h>
 #include "estruturas.h"
 #include <vector>
 #include "trabalho/Movement.h"
 #include <iostream>
 
 bool waitingCallback = false;
-Comando command = CONTINUAR;
+int command = 0;
+
+// callback que recebe o comando processado pelo img_listener e salva o comando e atualiza o waitingCallback
+void yameteCallback(const std_msgs::Int64::ConstPtr& msg){
+    std::cout << "Comando recebido: " << command << std::endl;
+    command = msg->data;
+    waitingCallback = false;
+}
+
+void PrintMap(int mapa[][13], Posicao pos) {
+
+    const static char values[5] = {' ', 'F', 'E', 'D', 'X'};
+
+    for (int b=0; b<12; b++) {
+        for (int a=0; a<12; a++) {
+            if (a == pos.x && b == pos.y) {
+                std::cout << "C";
+            } else {
+                int value = mapa[b][a];
+                std::cout << values[value];
+            }
+            std::cout << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+}
 
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "robot");
+
     ros::NodeHandle nh;
-    ros::ServiceClient client = nh.serviceClient<trabalho::Movement>("/movement");
     
+    ros::Subscriber sub = nh.subscribe<std_msgs::Int64>("result", 1, yameteCallback);
+    ros::ServiceClient client = nh.serviceClient<trabalho::Movement>("/movement");
     image_transport::ImageTransport it(nh);
     image_transport::Publisher pub = it.advertise("camera/image", 1);
 
@@ -24,8 +53,6 @@ int main(int argc, char **argv) {
     imagens.push_back(cv::imread("/home/willian/catkin_ws/src/trabalho/src/images/frente.png", cv::IMREAD_COLOR));
     imagens.push_back(cv::imread("/home/willian/catkin_ws/src/trabalho/src/images/esquerda.png", cv::IMREAD_COLOR));
     imagens.push_back(cv::imread("/home/willian/catkin_ws/src/trabalho/src/images/direita.png", cv::IMREAD_COLOR));
-    
-    char values[5] = {' ', 'F', 'E', 'D', 'X'};
     
     int mapa[13][13] = {
         {0, 2, 0, 3, 0, 2, 0, 1, 0, 2, 0, 3, 0},
@@ -51,42 +78,40 @@ int main(int argc, char **argv) {
     posicao.y = 2;
 
     ros::Rate loop_rate(1);
-    ros::Rate loop_rate_waiting(0.1);
+    ros::Rate loop_rate_waiting(1);
 
     sensor_msgs::ImagePtr msg;
 
-
-
     for (int i=0; i<12 && nh.ok(); i++) {
+        
+        int theImage = mapa[posicao.y][posicao.x];
+        msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", imagens.at(theImage)).toImageMsg();
 
-        ros::spinOnce();
+        waitingCallback = true;
+        pub.publish(msg);
+
+        while(waitingCallback) {
+            ros::spinOnce();
+            std::cout << "Aguardando callback.." << std::endl;
+            loop_rate_waiting.sleep();
+        }
+
 
         if (posicao.x < 0 || posicao.y < 0 || posicao.x > 12 || posicao.y > 12) {
             ROS_INFO("ERRO FATAL: Robot is in the void.");
             return 0;
         }
 
-        for (int b=0; b<12; b++) {
-            for (int a=0; a<12; a++) {
-                if (a == posicao.x && b == posicao.y) {
-                    std::cout << "C";
-                } else {
-                    int value = mapa[b][a];
-                    std::cout << values[value];
-                }
-                std::cout << ' ';
-            }
-            std::cout << std::endl;
-        }
         std::cout << "------ Iteracao: " << i << " ------" << std::endl;
+        PrintMap(mapa, posicao);
         
         trabalho::Movement srv;
-        srv.request.cmd = mapa[posicao.y][posicao.x];
+        srv.request.cmd = command;
         srv.request.dir = direcao;
         srv.request.x = posicao.x;
         srv.request.y = posicao.y;
 
-        if(client.call(srv)){
+        if (client.call(srv)) {
             direcao = srv.response.dir;
             posicao.x = srv.response.x;
             posicao.y = srv.response.y;
